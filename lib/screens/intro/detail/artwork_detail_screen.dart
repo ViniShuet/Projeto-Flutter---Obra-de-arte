ebcaf8e60c3d90219fd5f4827b027d1c4a04a3c0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-12 20:00:31 -0300

    adicionando telas

---
+++ lib/screens/intro/detail/artwork_detail_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class ArtworkDetailScreen extends StatefulWidget {
+  final Artwork artwork;
+
+  const ArtworkDetailScreen({super.key, required this.artwork});
+
+
+  @override
+  State<ArtworkDetailScreen> createState() => _ArtworkDetailScreenState();
+}
+
+class _ArtworkDetailScreenState extends State<ArtworkDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
